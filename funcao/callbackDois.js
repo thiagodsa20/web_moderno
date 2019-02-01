4a3c03d6771d32eb6aae35d249b7c6ffbd6ea249
Thiago Dantas <thiagodsazevedo@gmail.com>
2019-02-01 11:13:11 -0200

    Função

--- funcao/callbackDois.js
+++ funcao/callbackDois.js
@@ -16,3 +16,7 @@ const notasBaixas2 = notas.filter(function (nota){
 })
 
 console.log(notasBaixas2)
+
+const notasMenorQue7 = nota => nota < 7
+const notasBaixas3 = notas.filter(nota => nota < 7)
+console.log(notasBaixas3)
